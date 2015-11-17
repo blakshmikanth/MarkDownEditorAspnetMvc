6c79286219d6037e3cba4eafb84f55a33845975b
vendettamit <vendett.amit@gmaill.com>
2015-11-16 19:57:23 -0500

    Fixed issue #1. Added github fork ribben.

--- MarkDownMvcEditor/Models/DefaultModel.cs
+++ MarkDownMvcEditor/Models/DefaultModel.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MarkDownMvcEditor.Models
 {
     public class DefaultModel
     {
+        [AllowHtml]
         public string PostMarkdown { get; set; }
     }
 }
